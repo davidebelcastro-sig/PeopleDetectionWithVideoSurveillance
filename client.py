979b9039f774623035c681b6db60a444e2706580
Davide Belcastro <117814083+davidebelcastro-sig@users.noreply.github.com>
2023-05-17 14:27:05 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,8 +1,10 @@
 #import socket
 import cv2
 from PIL import Image
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
+import time
 
 address = ""
 fp = open("file_configurazione.txt","r")
@@ -12,22 +14,21 @@ for riga in fp:
         address = ls[1][:-1]
    
 fp.close()
-browser = webdriver.Chrome()
+chrome_options = Options()
+service = Service('./chromedriver')
+browser = webdriver.Chrome(service=service, options=chrome_options)
 browser.get(address)
 
-volt=0
+
 while 1:
     browser.save_screenshot('salvata.png')
-    if volt == 0:
-        im = Image.open('salvata.png')
-        img = im.crop((50,250,715,650))
-        img.save('salvata2.png')
-        f = open("appo","w")
-        f.close()
-        print("invio")
-    if volt == 33: #15
-        volt=-1
-    volt+=1
+    im = Image.open('salvata.png')
+    img = im.crop((50,250,715,650))
+    img.save('salvata2.png')
+    f = open("appo","w")
+    f.close()
+    print("invio")
+    time.sleep(2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cv2.destroyAllWindows()
